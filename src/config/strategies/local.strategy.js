3bbcc63ccc3249938ab91bcdfb99f65b9bf6b1c0
דוד שוסטרמן <davidshusti@gmail.com>
2017-02-04 19:24:01 +0200

    finished user authentication

--- src/config/strategies/local.strategy.js
+++ src/config/strategies/local.strategy.js
@@ -1,16 +1,29 @@
-var passport = require('passport');
-LocalStrategy = require('passport-local').Strategy;
+var passport = require('passport'),
+    LocalStrategy = require('passport-local').Strategy,
+    mongodb = require('mongodb').MongoClient;
 
-module.exports = function(){
+
+
+module.exports = function () {
     passport.use(new LocalStrategy({
-        usernameField: 'userName',
-        passwordField: 'password'
-    },
-    function (username,password,done) {
-        var user = {
-            username:username,
-            password:password
-        };
-        done(null,user);
-    }));
+            usernameField: 'userName',
+            passwordField: 'password'
+        },
+        function (username, password, done) {
+            var url = 'mongodb://localhost:27017/libraryApp';
+            mongodb.connect(url, function (err, db) {
+                var collection = db.collection('users');
+                collection.findOne({
+                    username: username
+                }, function (err, result) {
+                    if (result.password === password) {
+                        var user = result;
+                        done(null, user);
+                    }else{
+                        done(null,false);
+                    }
+
+                });
+            });
+        }));
 };
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var authRouter = express.Router();
 var mongodb = require('mongodb').MongoClient;
-
+var passport = require('passport');
 
 var router = function () {
     authRouter.route('/signUp')
@@ -16,14 +16,27 @@ var router = function () {
                     password: req.body.password
                 };
                 collection.insert(user, function (err, results) {
-                    req.login(results , function () {
+                    req.login(results.ops[0], function () {
                         res.redirect('/auth/profile');
                     });
                 });
             });
         });
 
+    authRouter.route('/signIn')
+        .post(passport.authenticate('local', {
+            
+        }), function (req, res) {
+            res.redirect('/auth/profile');
+        });
+
     authRouter.route('/profile')
+        .all(function(req,res,next){
+            if (!req.user){
+                res.redirect('/');
+            }
+            next();
+        })
         .get(function (req, res) {
             res.json(req.user);
         });
